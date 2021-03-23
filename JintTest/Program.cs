f26f0de4050e6ec2a180f2f02264f6e02e512328
Bernhard Windisch <bernhard.windisch@doob.at>
2021-03-23 14:36:00 +0100

    Add project files.

---
+++ JintTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Jint;
+
+namespace JintTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new Engine(opts =>
+            {
+                opts.AddExtensionMethods(typeof(StringExtensions));
+            });
+
+            var arr = engine.Execute("'yes,no'.split(',')").GetCompletionValue().AsArray();
+
+
+        }
+    }
+}
